92490bc3c6c5f342dad4a873b3acebbda2686d00
shivangi <shivangisoti18@gmail.com>
2026-04-24 19:53:24 +0530

    Ready boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <h1>React Interview Project</h1>
+        <p>Ready for implementation</p>
+        
+        <Routes>
+          {/* Routes will be added during interview */}
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
